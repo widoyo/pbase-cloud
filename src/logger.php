884c32155a130ea90649daa362d651658a531e31
rpahlevy <rio.pahlevy@gmail.com>
2019-12-10 19:50:57 +0700

    fix UI & some logic

--- src/logger.php
+++ src/logger.php
@@ -35,21 +35,21 @@ $app->group('/logger', function () use ($getLoggerMiddleware) {
             if ($raw) {
                 $raw['content'] = json_decode($raw['content']);
 
-                if (isset($raw['content']->temperature) && !empty($logger['temp_cor'])) {
-                    $raw['content']->temperature += $logger['temp_cor'];
-                }
+                // if (isset($raw['content']->temperature) && !empty($logger['temp_cor'])) {
+                //     $raw['content']->temperature += $logger['temp_cor'];
+                // }
 
-                if (isset($raw['content']->humidity) && !empty($logger['humi_cor'])) {
-                    $raw['content']->humidity += $logger['humi_cor'];
-                }
+                // if (isset($raw['content']->humidity) && !empty($logger['humi_cor'])) {
+                //     $raw['content']->humidity += $logger['humi_cor'];
+                // }
 
-                if (isset($raw['content']->battery) && !empty($logger['batt_cor'])) {
-                    $raw['content']->battery += $logger['batt_cor'];
-                }
+                // if (isset($raw['content']->battery) && !empty($logger['batt_cor'])) {
+                //     $raw['content']->battery += $logger['batt_cor'];
+                // }
 
-                if (isset($raw['content']->tick) && !empty($logger['tipp_fac'])) {
-                    $raw['content']->tick += $logger['tipp_fac'];
-                }
+                // if (isset($raw['content']->tick) && !empty($logger['tipp_fac'])) {
+                //     $raw['content']->tick += $logger['tipp_fac'];
+                // }
 
                 // if (isset($raw['content']->distance) && !empty($logger['ting_son'])) {
                 //     $raw['content']->distance += $logger['ting_son'];
@@ -181,21 +181,21 @@ $app->group('/logger', function () use ($getLoggerMiddleware) {
             foreach ($raws as $raw) {
                 $raw['content'] = json_decode($raw['content']);
 
-                if (isset($raw['content']->temperature) && !empty($logger['temp_cor'])) {
-                    $raw['content']->temperature += $logger['temp_cor'];
-                }
+                // if (isset($raw['content']->temperature) && !empty($logger['temp_cor'])) {
+                //     $raw['content']->temperature += $logger['temp_cor'];
+                // }
 
-                if (isset($raw['content']->humidity) && !empty($logger['humi_cor'])) {
-                    $raw['content']->humidity += $logger['humi_cor'];
-                }
+                // if (isset($raw['content']->humidity) && !empty($logger['humi_cor'])) {
+                //     $raw['content']->humidity += $logger['humi_cor'];
+                // }
 
-                if (isset($raw['content']->battery) && !empty($logger['batt_cor'])) {
-                    $raw['content']->battery += $logger['batt_cor'];
-                }
+                // if (isset($raw['content']->battery) && !empty($logger['batt_cor'])) {
+                //     $raw['content']->battery += $logger['batt_cor'];
+                // }
 
-                if (isset($raw['content']->tick) && !empty($logger['tipp_fac'])) {
-                    $raw['content']->tick += $logger['tipp_fac'];
-                }
+                // if (isset($raw['content']->tick) && !empty($logger['tipp_fac'])) {
+                //     $raw['content']->tick += $logger['tipp_fac'];
+                // }
 
                 // if (isset($raw['content']->distance) && !empty($logger['ting_son'])) {
                 //     $raw['content']->distance += $logger['ting_son'];
